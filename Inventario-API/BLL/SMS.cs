70448e4833c637e9ec3a76aac6dc959332891b3b
JuanYanez292 <57423609+JuanYanez292@users.noreply.github.com>
2024-06-25 18:42:08 -0600

    Update SMS.cs

--- Inventario-API/BLL/SMS.cs
+++ Inventario-API/BLL/SMS.cs
@@ -11,15 +11,15 @@ namespace BLL
 {
     public class SMS
     {
-        private const string AccountSid = "ACbd7fa196baf26722498e0687c6896886";
-        private const string AuthToken = "99c7b08cd87d8151fb34468026ab31eb";
+        private const string AccountSid = "";
+        private const string AuthToken = "";
 
         public static void EnviarMensaje()
         {
             TwilioClient.Init(AccountSid, AuthToken);
 
-            var toPhoneNumber = "+528136183555";
-            var fromPhoneNumber = "+19386661795";
+            var toPhoneNumber = "";
+            var fromPhoneNumber = "";
             var messageBody = "Un producto ha sido eliminado";
 
             var messageOptions = new CreateMessageOptions(new PhoneNumber(toPhoneNumber))
